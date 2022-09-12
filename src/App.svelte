34747fb7a75d482980e5f505771bd69d67a2378a
ouarrho oualid <34518781+ouarrho@users.noreply.github.com>
2022-09-12 11:22:31 +0200

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+
+  import Card from './components/Card.svelte'
+
+</script>
+
+<main class="d-flex align-items-center" style="height: 100vh;margin-top: -50px;">
+
+	<div class="container p-4">
+
+		<Card />
+
+	</div>
+
+</main>
